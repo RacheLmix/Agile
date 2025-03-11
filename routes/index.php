3a679d51599a103f9ca61eb973469706261550ac
unknown <tungxeko912@gmail.com>
2025-03-11 16:31:22 +0700

    Rin

--- routes/index.php
+++ routes/index.php
@@ -6,10 +6,12 @@ $router = new \Bramus\Router\Router();
 $router->mount('', function () use ($router) {
 
     $router->get('/', function () {
-        echo "Hello World";
+        view('layout.layout');
     });
     //viết tiếp Router ở dưới!!
-
+    $router->mount('/users', function () use ($router){
+        $router->get('/index', UserControllers::class . '@index');
+    });
 });
 
 // Run it!
