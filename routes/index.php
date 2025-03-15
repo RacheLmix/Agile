48f2fdacc80eec97acc97b31a2641d23b6690926
Rachel <tungxeko912@gmail.>
2025-03-15 23:53:49 +0700

    Rachel

--- routes/index.php
+++ routes/index.php
@@ -3,9 +3,6 @@ use App\Controllers\admin\BookingController;
 // Create Router instance
 $router = new \Bramus\Router\Router();
 
-require_once "web.php";
-require_once "admin.php";
-
 // viết router ở đây
 $router->mount('', function () use ($router) {
 
@@ -14,7 +11,9 @@ $router->mount('', function () use ($router) {
     });
     //viết tiếp Router ở dưới!!
     $router->mount('/admin', function () use ($router) {
+        $router->mount('/dashboard', function () use ($router) {
         $router->get('/booking', BookingController::class . '@index');
+        });
     });
 
     $router->mount('/users', function () use ($router){});
